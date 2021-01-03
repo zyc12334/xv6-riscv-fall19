fd3df732a17bd3bd94ad4ccb4ce84d8b72f581ef
zyc12334 <45460331+zyc12334@users.noreply.github.com>
2021-01-03 21:05:56 +0800

    Update sysproc.c

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -113,7 +113,7 @@ uint64 sys_sigalarm(void) {
 
 uint64 sys_sigreturn(void){
   struct proc *p = myproc();
-  p->cb_ticks = p->cb_interval;
+  p->cb_ticks -= p->cb_interval;
   p->cb_running = 0;
   *p->tf = p->cb_snapshot;
   return 0;
